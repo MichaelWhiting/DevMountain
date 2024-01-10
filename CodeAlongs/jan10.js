53390281fc9399dddf3a0b0852edf7f4c0b312b9
MichaelWhiting <michaelbwhiting2004@gmail.com>
2024-01-10 14:52:38 -0700

    finished arrays/looping lab

--- CodeAlongs/jan10.js
+++ CodeAlongs/jan10.js
@@ -1,1 +1,19 @@
-let name = "Michael"
+// const meals = ["pizza", "burger", "fries", "chicken"];
+
+// const lastMeal = meals.pop();
+
+// console.log(lastMeal);
+// console.log(meals);
+
+// let students = [];
+// students.push("Cat", "Ty", "Lincoln");
+
+// console.log(students.length);
+
+const perfTemp = 125
+let teaTemp = 130
+
+while (teaTemp > perfTemp) {
+    teaTemp -= 1
+    console.log(`Tea temperature: ${teaTemp}`)
+}
